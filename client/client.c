cb2df74ad82dbb33a0dd1b130f78601a482e8cf4
Florent <florentvilleneuve1@gmail.com>
2021-11-17 17:51:48 +0100

    Ajout des commentaires au client.c

--- client/client.c
+++ client/client.c
@@ -14,85 +14,95 @@
 #define NBR_CARACTERES 80
 #define NBR_PERSONNAGES 19
 
+
+//Structure permettant d'envoyer d'un coup les informations importantes du client vers le serveur
 typedef struct messageClientServeur {
-    int type_message;         //0 Initialisation et 1 pour le message final une fois le jeu fini
-    int resultat;  					  //0 si on n'a pas trouvé, 1 si on trouve -> ce parametre est utile uniquement si le message est de type 1 (message final de fin de partie)
+    int type_message;                                             //0 Initialisation et 1 pour le message final une fois le jeu fini
+    int resultat;  					                                     //1 si on trouve -> ce parametre est utile uniquement si le message est de type 1 (message final de fin de partie)
     pid_t pid;
     char identite_envoyeur [50];
 } MessageClientServeur;
 
 
+/*/Protorypes des fonctions/*/
 int menu (char personnageselect[NBR_CARACTERES], char tableau[NBR_PERSONNAGES ][NBR_CARACTERES]);
 void strcpy_pointeur(char *, char);
 int launch_regex(char *, char *);
 void affichagePersonnages(char tableau[NBR_PERSONNAGES ][NBR_CARACTERES]);
-void arretCTRLC(){exit(0);};
+void arretCTRLC();
 char** str_split(char* , const char);
 void personnageTrouve();
 void partieDejaCommence();
 int attenteTouche ( void );
 
-void clean_stdin(void)
-{
-    int c;
-    do {
-        c = getchar();
-    } while (c != '\n' && c != EOF);
-}
 
 int main(void){
 
-    signal(SIGINT, arretCTRLC); 
-    signal(SIGUSR1, personnageTrouve);
-    signal(SIGUSR2, partieDejaCommence);
+	/*/Gestion de tous les signaux reçus/*/
+    signal(SIGINT, arretCTRLC);                                       //Prise en compte du CTRL+C 
+    signal(SIGUSR1, personnageTrouve);                     //Signal reçu quand un adversaire trouve l'élève mystère avant nous
+    signal(SIGUSR2, partieDejaCommence);				//Signal reçu quand on essaie de se connecter au serveur alors que le nombre maxi de joueurs est atteint
 
+	/*/Création des variables nécessaires/*/
+    int descW, descR;                                                                                         //Descripteurs
+    char prenom[50];																							//Prénom de l'utilisateur de ce client
+    char tableau[NBR_PERSONNAGES ][NBR_CARACTERES];			//Tableau qui contiendra la base de données utilisée
+    char personnageselect[NBR_CARACTERES];										//Array qui contiendra le nom de l'élève mystère
+    chdir("../pipe");																								 //On se place dans le bon répertoire pour l'utilisation des pipes
 
-    int descW, descR;
-    char prenom[50];
-    char tableau[NBR_PERSONNAGES ][NBR_CARACTERES];
-    char personnageselect[NBR_CARACTERES];
-    
-    chdir("../pipe"); //Pour le faire fonctionner sur les autres machines
-
-    /* On demande le nom du client, qui est tu ?*/
-    printf("\e[1;1H\e[2J"); //Nettoie l'écran de la console
+    /*/ On demande le nom du client, qui est tu ?/*/
+    printf("\e[1;1H\e[2J"); 																							//Nettoie l'écran de la console
     printf("Bonjour et bienvenue dans le jeu \"Qui est-ce\" ?\n");
     printf("Quel est votre prenom ? (Exemples : Olivier, Arnaud...)\n");
     scanf("%s", prenom);
 
+	/*/On formalise la réponse en utilsant une structure MessageClientServeur/*/
     MessageClientServeur *messageInitialisation = malloc(sizeof(MessageClientServeur));
     messageInitialisation->type_message = 0;
     messageInitialisation->resultat = 0;
     messageInitialisation->pid = getpid();
     strcpy(messageInitialisation->identite_envoyeur, prenom);
 
-    /* penser à ajouter main entre ""*/
-    descW=open("main",O_WRONLY); // on ouvre le pipe main en ecriture
+    /*/On envoie la structure de message à notre serveur à travers le pipe "main"/*/
+    descW=open("main",O_WRONLY); 																					// on ouvre le pipe main en ecriture
     write(descW, messageInitialisation, sizeof(MessageClientServeur));
-    close(descW); // on ferme le descripteur
+    close(descW); 																														// on ferme le descripteur
     sleep(1);
 	
-    descR=open(prenom,O_RDONLY); // on ouvre le pipe main en lecture
+	/*/On attend la réponse du serveur qui nous donne la base de données et l'élève mystère/*/
+    descR=open(prenom,O_RDONLY); 
     read(descR, tableau, sizeof(char)*NBR_PERSONNAGES*NBR_CARACTERES);
     read(descR, personnageselect, sizeof(char)*NBR_CARACTERES);
 	close(descR);
     
     printf("\nBase de données et élève mystère chargés !\nAttente du lancement de la partie !\n");
 	
+	
+	/*/On lance le menu qui contient toute la partie Jeu/*/
     int resultat = menu(personnageselect, tableau);
 
+	/* NOTE :
+	 *  Le résultat donné par menu ne peut être que 1, ce qui correspond a une partie gagné
+	 *  On peut éventuellement penser à un système de pénalité qui se finirait par l'exclusion de la partie le joueur
+	 *  Ici ce n'est pas pris en compte
+	 */
 
+
+	/*/On exploite le résultat de la partie donné par menu()/*/
     if (resultat == 1){
-
+		//On refait un message avec notre structure...
         MessageClientServeur *messageFinal = malloc(sizeof(MessageClientServeur));
         messageFinal->type_message = 1;
         messageFinal->resultat = resultat;
         messageFinal->pid = getpid();
         strcpy(messageFinal->identite_envoyeur, prenom);
 
+		//On l'envoie dans le pipe
         descW=open("main",O_WRONLY);
         write(descW, messageFinal,sizeof(MessageClientServeur));
-        close(descW); // on ferme le descripteur
+        close(descW); 
+        
+        //On ferme ce client
         printf("\nFin du client de %s!\n", prenom);
         printf("Au revoir !\n");
         exit (1);
@@ -120,7 +130,7 @@ int menu(char personnageselect[NBR_CARACTERES], char tableau[NBR_PERSONNAGES][NB
     }
     
     do {
-        //menu global       
+        //Menu global       
         printf("\e[1;1H\e[2J");
         printf("\n********** Bienvenue dans le menu du jeu QUI EST-CE ? **********\n");
         printf("|  0 | Quitter le programme\n");
@@ -134,8 +144,8 @@ int menu(char personnageselect[NBR_CARACTERES], char tableau[NBR_PERSONNAGES][NB
                 exit(0);
                 break;
             case 1:
-                printf("\e[1;1H\e[2J"); //Nettoie l'écran de la console
-                //menu des caracteristiques
+                printf("\e[1;1H\e[2J");                             //Nettoie l'écran de la console
+                //Menu des caracteristiques
                 printf("|  1 | Couleur des yeux\n");
                 printf("|  2 | Couleur des cheveux\n");
                 printf("|  3 | Regularité à l'apéro\n");
@@ -145,7 +155,7 @@ int menu(char personnageselect[NBR_CARACTERES], char tableau[NBR_PERSONNAGES][NB
                     case 0:
                         break;
                     case 1:
-                        //menu des yeux
+                        //Menu des yeux
                         printf("|  1 | Vert\n");
                         printf("|  2 | Lunettes\n");
                         printf("|  3 | Marron\n");
@@ -180,7 +190,7 @@ int menu(char personnageselect[NBR_CARACTERES], char tableau[NBR_PERSONNAGES][NB
 
                         break;
                     case 2:
-                        //menu des cheveux
+                        //Menu des cheveux
                         printf("|  1 | Chatain\n");
                         printf("|  2 | Grisonant\n");
                         printf("|  3 | Brun\n");
@@ -216,7 +226,7 @@ int menu(char personnageselect[NBR_CARACTERES], char tableau[NBR_PERSONNAGES][NB
 
                         break;
                     case 3:
-                        //menu de la regularite à l'apéro
+                        //Menu de la regularite à l'apéro
                         printf("|  1 | Faible\n");
                         printf("|  2 | Moyen\n");
                         printf("|  3 | Fort\n");
@@ -281,13 +291,13 @@ int menu(char personnageselect[NBR_CARACTERES], char tableau[NBR_PERSONNAGES][NB
                 }
                 break;
             case 2:
-				printf("\e[1;1H\e[2J"); //Nettoie l'écran de la console
+				printf("\e[1;1H\e[2J");
 				
                 printf("Quel est le nom (MAJUSCULE) ou le prenom (minuscule) de l'élève mystère ?\n");
                 scanf("%s", chaine_recherche);
                 status = launch_regex(elu, chaine_recherche);
 
-                if ((status == 0) || (status ==1)) {     //C'est surement le if le plus chelou que j'ai vu, faut le changer ou le prof se moquera de nous.
+                if ((status == 0) || (status ==1)) {    
                     if (status == 0) {
                         printf("Personnage trouvé %s ! C'est gagné !\n", elu);
                         printf("Appuyer sur une touche pour continuer.\n");
@@ -308,11 +318,11 @@ int menu(char personnageselect[NBR_CARACTERES], char tableau[NBR_PERSONNAGES][NB
                 }
                 break;
             case 3:
-				printf("\e[1;1H\e[2J"); //Nettoie l'écran de la console
+				printf("\e[1;1H\e[2J"); 
                 affichagePersonnages(tableau);
                 printf("\nAppuyer sur une touche pour continuer.\n");
 				attenteTouche();
-				attenteTouche();
+				attenteTouche();          //Ce double attenteTouche() est la solution bête et méchante que j'ai trouvé, affichagePersonnages() donne déjà un caractère à la fin de son exécution du à l'utilisation de puts je présume
                 break;
             default:
                 break;
@@ -324,6 +334,10 @@ int menu(char personnageselect[NBR_CARACTERES], char tableau[NBR_PERSONNAGES][NB
 }
 
 int launch_regex(char *elu, char *p_chaine_recherche){
+	/* Permet de comparer l'élève mystère au nom rentré par le joueur 
+	 *  Retourne vrai ou faux
+	 */
+	 
     regex_t regex;
     int reti;
     char msgbuf[100];
@@ -348,7 +362,7 @@ int launch_regex(char *elu, char *p_chaine_recherche){
     }
     else {
         regerror(reti, &regex, msgbuf, sizeof(msgbuf));
-        fprintf(stderr, "Regex match failed: %s\n", msgbuf);
+        fprintf(stderr, "Echec Regex: %s\n", msgbuf);
         status = 2;
         exit(1);
     }
@@ -360,6 +374,10 @@ int launch_regex(char *elu, char *p_chaine_recherche){
 
 
 void affichagePersonnages(char tableau[NBR_PERSONNAGES ][NBR_CARACTERES]){
+	/* Fonction permettant au joueur de voir la liste des élèves et de leurs caractéristiques
+	 *  C'est fonctionnel mais peu esthétique, on continue d'avoir les séparateurs ':' de la base de données
+	 *  Il faudrait refaire cette fonction en utilisant des tokens pour avoir un affichage plus plaisaint
+	 */
 
     printf("Liste des Personnages et de leurs caractéristiques: \n");
     for(int i = 0; i < NBR_PERSONNAGES ; i++)
@@ -407,6 +425,8 @@ char** str_split(char* a_str, const char a_delim) {
 }
 
 void personnageTrouve() {
+	/* Cas où un adversaire trouve la réponse avant nous. 
+	 */
     printf("\nUn de vos adversaires vous a devancé et a trouvé l'élève caché !\nLa carte Olimex vous dévoilera de qui il s'agit !\n");
 	printf("\nAppuyer sur une touche pour continuer.\n");
 	attenteTouche();
@@ -415,13 +435,18 @@ void personnageTrouve() {
 
 
 void partieDejaCommence(){
+	/*Cas où le nombre de joueur max est atteint sur le serveur et qu'on ne peut plus se connecter
+	 */
 	printf("\nLa partie a déjà commencé ! Impossible de participer pour le moment, réessayer plus tard s'il vous plaît.\n");
 	exit(0);
 }
 
 
-int attenteTouche ( void ) 
-{
+int attenteTouche ( void ) {
+	/* Fonction que Florent VILLENEUVE a volé sur internet de manière complétement éhonté
+	 *  Elle sert dans les menus à attendre avant de retourner au "menu global" (premier switch case)
+	 *  Sinon le joueur n'aurait pas le temps de voir ce qu'il s'affiche
+	 */
   int ch;
   struct termios oldt, newt;
   tcgetattr ( STDIN_FILENO, &oldt );
@@ -431,4 +456,10 @@ int attenteTouche ( void )
   ch = getchar();
   tcsetattr ( STDIN_FILENO, TCSANOW, &oldt );
   return ch;
+}
+
+
+void arretCTRLC(){
+	//Sert sans doute à rien, c'est un reste de notre première itération ou les clients étaient des fils du serveur.
+	exit(0);
 }
